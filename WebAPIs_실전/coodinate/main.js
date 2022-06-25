1a16cafb3c6cade90c96592b71203c2237248805
Beomjin Park <pbj0556@jbnu.ac.kr>
2022-06-25 10:54:28 +0900

    DOM 기초

--- WebAPIs_실전/coodinate/main.js
+++ WebAPIs_실전/coodinate/main.js
@@ -3,12 +3,26 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-window.addEventListener('mousemove', (e) => {
-  horizontal.style.top = `${e.clientY}px`;
-  vertical.style.left = `${e.clientX}px`;
-  target.style.left = `${e.clientX}px`;
-  target.style.top = `${e.clientY}px`;
-  tag.style.left = `${e.clientX}px`;
-  tag.style.top = `${e.clientY}px`;
-  tag.innerHTML = `${e.clientX}px, ${e.clientY}px`;
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
