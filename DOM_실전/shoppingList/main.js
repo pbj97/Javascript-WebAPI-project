75beb2703941b7a978418af6a8745027925aeebc
Beomjin Park <pbj0556@jbnu.ac.kr>
2022-06-28 22:50:48 +0900

    게임만들기 game stop까지

--- DOM_실전/shoppingList/main.js
+++ DOM_실전/shoppingList/main.js
@@ -1,6 +1,12 @@
 const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  onAdd();
+});
 
 function onAdd() {
   const text = input.value;
@@ -20,41 +26,27 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const name = document.createElement('sapn');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+    <div class="item" data-id=${id}>
+      <span class="item__name">${text}</span>
+      <button class="item__delete">
+        <i class="fa-solid fa-trash-can" data-id=${id}></i>
+      </button>
+    </div>
+    <div class="item__divider"></div>`;
+  id++;
   return itemRow;
 }
 
-addBtn.addEventListener('click', () => {
-  onAdd();
-});
-
-input.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    onAdd();
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
